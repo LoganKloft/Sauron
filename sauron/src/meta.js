9073a5561bdbaf73278ede77f2ab34050ceee29c
Logan Kloft <lkloft@outlook.com>
2023-11-12 15:26:17 -0800

    Automatically create ./src/data/query folder if it doesn't exist

--- sauron/src/meta.js
+++ sauron/src/meta.js
@@ -1,5 +1,9 @@
-const { existsSync, mkdirSync, copyFileSync } = require('fs');
+const { existsSync, mkdirSync } = require('fs');
 
 export function init() {
-
+    // check if ./src/data/query exists
+    // if not create it
+    if (!existsSync('./src/data/query')) {
+        mkdirSync('./src/data/query');
+    }
 }
