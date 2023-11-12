756f6d09dddc83082f26a6975b2315c5a15689f7
Logan Kloft <lkloft@outlook.com>
2023-11-11 18:21:02 -0800

    Create basic input elements for creating a task. Error with native 'fs' module

--- sauron/src/pages/home/index.jsx
+++ sauron/src/pages/home/index.jsx
@@ -1,7 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button'
 import Link from '@mui/material/Link'
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
 
 function Home() {
 
@@ -10,6 +12,9 @@ function Home() {
     return (
         <div>
             <Button variant="contained" onClick={() => navigate('upload')}>Upload</Button>
+            <Fab color="primary" aria-label="add" onClick={() => navigate('upload')}>
+                <AddIcon />
+            </Fab>
             <Link href="https://icons8.com/icon/41528/one-ring">Ring</Link> icon by <Link href="https://icons8.com">Icons8</Link>
         </div>
     )
--- sauron/src/pages/upload/index.jsx
+++ sauron/src/pages/upload/index.jsx
@@ -1,14 +1,92 @@
-import React from 'react'
+import React, { useRef, useState } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button'
+import TextField from '@mui/material/TextField';
+import { styled } from '@mui/material/styles';
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Switch from '@mui/material/Switch';
+import Typography from '@mui/material/Typography';
+import Yolo from './yolo.jsx';
+
+import { readFile, writeFile } from 'fs/promises';
+
+const VisuallyHiddenInput = styled('input')({
+    clip: 'rect(0 0 0 0)',
+    clipPath: 'inset(50%)',
+    height: 1,
+    overflow: 'hidden',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    whiteSpace: 'nowrap',
+    width: 1,
+});
 
 function Upload() {
 
     const navigate = useNavigate();
 
+    const [name, setName] = useState('');
+    const [showYoloParams, setShowYoloParams] = useState(true);
+    const [sourceFileLocation, setSourceFileLocation] = useState('');
+    const [sourceFileName, setSourceFileName] = useState('');
+
+    const modelParametersRef = useRef({ "yolo": {} });
+
+    function handleFileUpload(event) {
+        if (event.target.files.length !== 0) {
+            modelParametersRef.current["yolo"]["source"] = event.target.files[0].path
+            setSourceFileLocation(event.target.files[0].path);
+            setSourceFileName(event.target.files[0].name);
+        }
+    }
+
+    function handleNameChange(event) {
+        modelParametersRef.current["yolo"]["name"] = event.target.value;
+        setName(event.target.value);
+    }
+
+    async function handleCreateTask() {
+        const models = []
+        if (showYoloParams) models.push("yolo");
+
+        const task = {}
+
+        task["source_file_location"] = sourceFileLocation;
+        task["source_file_name"] = sourceFileName;
+        task["create_date"] = (new Date()).toUTCString();
+        task["models"] = models;
+        task["status"] = "unprocessed";
+        task["model_params"] = modelParametersRef.current;
+
+        const tasks = JSON.parse(
+            await readFile("../../data/task/tasks.json"));
+
+        const index = tasks["count"];
+        tasks["count"] = tasks["count"] + 1;
+        tasks[index] = task;
+
+        await writeFile("../../data/task/tasks.json", JSON.stringify(tasks));
+
+        navigate('/');
+    }
+
     return (
         <div>
+            <Button component="label" variant="contained" startIcon={<CloudUploadIcon />}>
+                Upload Video
+                <VisuallyHiddenInput type="file" onChange={(e) => handleFileUpload(e)} />
+            </Button>
+            <TextField id="task-name" label="Task name" variant="standard" onChange={(e) => handleNameChange(e)} />
+            <Typography>Model Selection</Typography>
+            <FormGroup>
+                <FormControlLabel control={<Switch defaultChecked onClick={() => setShowYoloParams((prev) => !prev)} />} label="Yolo" />
+            </FormGroup>
+            {showYoloParams && <Yolo modelParametersRef={modelParametersRef} />}
             <Button variant="contained" onClick={() => navigate('/')}>Home</Button>
+            <Button variant="contained" onClick={() => handleCreateTask()}>Create Task</Button>
         </div>
     )
 }
